af6a4adcc6bdde09cf1972ff84806bf5eaa025aa
Keil-ENZO <enzo.keil06@icloud.com>
2025-03-15 19:40:20 +0100

    fix bug deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config: import("webpack").Configuration) => {
     // Configuration pour les fichiers GLSL
+    if (!config.module) config.module = { rules: [] };
+    if (!config.module.rules) config.module.rules = [];
+
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
       exclude: /node_modules/,
